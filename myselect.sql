84b32ef78831536e9e1a7f551be56ff9b7a6c30c
durongze <durongze@qq.com>
2019-07-06 13:05:41 +0100

    inner join and outer join

--- myselect.sql
+++ myselect.sql
@@ -10,6 +10,8 @@ insert into chinese values('c', 21);
 insert into chinese values('d', 31);
 insert into chinese values('e', 41);
 insert into chinese values('f', 51);
+insert into chinese values('l', 71);
+insert into chinese values('m', 81);
 --math table
 create table math (name char, score int);
 insert into math values('a',92);
@@ -18,3 +20,14 @@ insert into math values('c',22);
 insert into math values('d',32);
 insert into math values('e',42);
 insert into math values('f',52);
+insert into math values('u',72);
+insert into math values('v',82);
+--left join
+select c.name,c.score,m.score from chinese as c left join math as m on c.name=m.name;
+--right join
+select m.name,c.score,m.score from chinese as c right join math as m on c.name=m.name;
+--inner join
+select c.name,c.score,m.score from chinese as c inner join math as m on c.name=m.name;
+--outer join
+select c.name,c.score,m.score from chinese as c left join math as m on c.name=m.name union select m.name,c.score,m.score from chinese as c right join math as m on c.name=m.name;
+
