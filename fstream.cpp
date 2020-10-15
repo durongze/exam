0a0004db0311ec9ec97066f33edb1f3c0c71e20a
durongze <durongze@qq.com>
2020-10-15 09:03:29 +0800

    Update fstream.cpp

--- fstream.cpp
+++ fstream.cpp
@@ -281,3 +281,236 @@ int main(int argc, char* argv[])
     return 0;
 }
 
+int mainGetMaxSubArr()
+{
+	int idx = 0;
+	int cnt = 0;
+	int number = 0;
+	std::vector<int> arr;
+
+	std::cin >> cnt;
+
+	for (idx = 0; idx < cnt; ++idx)
+	{
+		std::cin >> number;
+		arr.push_back(number);
+	}
+
+	std::cout << getMaxSubArr(arr) << std::endl;
+	return 0;
+}
+
+int GenTxtByLrc(const std::string& fileTxt, const std::string& fileLrc)
+{
+	char oper = ']';
+	std::map<int, std::vector<std::string> > data;
+	ParseFile(fileLrc, data, oper);
+
+	std::ofstream fs(fileTxt, std::ios::out | std::ios::trunc);
+	std::map<int, std::vector<std::string> >::iterator iterData;
+	for (iterData = data.begin(); iterData != data.end(); ++iterData) {
+		std::vector<std::string>::iterator iterWord;
+		for (iterWord = iterData->second.begin(), ++iterWord; iterWord != iterData->second.end(); ++iterWord)
+		{
+			fs << *iterWord << oper;
+		}
+		fs << std::endl;
+	}
+	return 0;
+}
+
+int WriteLrcTable(std::map<int, std::vector<std::string> >& data, std::ofstream& fs)
+{
+	std::map<int, std::vector<std::string> >::iterator iterData;
+	for (iterData = data.begin(); iterData != data.end(); ++iterData)
+	{
+		std::vector<std::string>::iterator iterWord;
+		for (iterWord = iterData->second.begin(); iterWord != iterData->second.end(); ++iterWord)
+		{
+			fs << *iterWord << " ";
+		}
+		fs << std::endl;
+	}
+	return 0;
+}
+#ifdef _WIN32
+#include <io.h>
+int ReadFileByDir(std::string dirName, std::string fileExt, std::vector<std::string>& files)
+{
+	struct _finddata_t s_file;
+	std::string str = dirName + "\\*" + fileExt;
+	bool withDir = false;
+	intptr_t h_file = _findfirst(str.c_str(), &s_file);
+	if (h_file != static_cast<intptr_t>(-1.0)) {
+		do {
+			if (withDir) {
+				files.push_back(dirName + "\\" + s_file.name);
+			} else {
+				files.push_back(std::string(s_file.name));
+			}
+		} while (_findnext(h_file, &s_file) == 0);
+	}
+	_findclose(h_file);
+	return 0;
+}
+#endif
+wchar_t * char2wchar(const char* cchar)
+{
+	wchar_t *m_wchar;
+	int len = MultiByteToWideChar(CP_ACP, 0, cchar, strlen(cchar), NULL, 0);
+	m_wchar = new wchar_t[len + 1];
+	MultiByteToWideChar(CP_ACP, 0, cchar, strlen(cchar), m_wchar, len);
+	m_wchar[len] = '\0';
+	return m_wchar;
+}
+char * wchar2char(const wchar_t* wchar)
+{
+	char * m_char;
+	int len = WideCharToMultiByte(CP_ACP, 0, wchar, wcslen(wchar), NULL, 0, NULL, NULL);
+	m_char = new char[len + 1];
+	WideCharToMultiByte(CP_ACP, 0, wchar, wcslen(wchar), m_char, len, NULL, NULL);
+	m_char[len] = '\0';
+	return m_char;
+}
+
+#include <mmSystem.h>
+#pragma comment(lib, "winmm.lib")
+
+MCI_OPEN_PARMS m_mciOpen;  //打开参数
+MCI_PLAY_PARMS m_mciPlay;  //播放参数
+
+bool PlayMP3(const char * szFileName)
+{
+	// PlaySound(szFileName, NULL, SND_FILENAME | SND_ASYNC|SND_LOOP);
+	m_mciOpen.lpstrDeviceType = TEXT("mpegvideo"); //要操作的文件类型
+	m_mciOpen.lpstrElementName = char2wchar(szFileName); //要操作的文件路径
+	MCIERROR mcierror = mciSendCommand(0, MCI_OPEN, MCI_OPEN_TYPE | MCI_OPEN_ELEMENT, (DWORD)&m_mciOpen); //打开文件命令
+	if (mcierror) {
+		return false;
+	}
+	else {
+		m_mciPlay.dwCallback = (DWORD)NULL;
+		m_mciPlay.dwFrom = (DWORD)0; //播放起始位置ms为单位
+		mciSendCommand(m_mciOpen.wDeviceID, MCI_PLAY, MCI_NOTIFY | MCI_FROM, (DWORD)(LPVOID)&m_mciPlay);
+	}
+
+	delete[] m_mciOpen.lpstrElementName;
+	return true;
+}
+
+int GetMp3TimeLen(const char *file)
+{
+	long lLength = 0;
+	wchar_t sPosition[MAX_PATH] = { 0 };
+	wchar_t *pCmd = NULL;
+	std::string cmd;
+	
+	cmd = "open ";
+	cmd += file;
+	cmd += " alias song";
+	pCmd = char2wchar(cmd.c_str());
+	mciSendString(pCmd, NULL, 0, NULL);
+	delete[] pCmd;
+
+	cmd = "status ";
+	cmd += file;
+	cmd += " length";
+	pCmd = char2wchar(cmd.c_str());
+	mciSendString(pCmd, sPosition, 255, 0);
+	delete[] pCmd;
+
+	std::wcout << sPosition << std::endl;
+	char *pPosition = wchar2char(sPosition);
+	lLength = strtol(pPosition, NULL, 10) / 1000;
+	std::cout << lLength << std::endl;
+	// mciSendString(TEXT("play song wait"), NULL, 0, NULL);
+	mciSendString(TEXT("close song"), NULL, 0, NULL);
+	delete[] pPosition;
+
+	return lLength;
+}
+
+int ShowItem(std::map<int, std::vector<std::string> >& item)
+{
+	std::string itemKey = "filename";
+	std::map<int, std::vector<std::string> >::iterator iterData;
+	for (iterData = item.begin(); iterData != item.end(); ++iterData) {
+		std::vector<std::string>::iterator iterWord;
+		iterWord = iterData->second.begin();
+		if (*iterWord == itemKey) {
+			++iterWord;
+			std::cout << *iterWord << std::endl;
+		}
+	}
+	return 0;
+}
+
+int ShowElement(std::vector<std::map<int, std::vector<std::string> > >& elements)
+{
+	std::vector<std::map<int, std::vector<std::string> > >::iterator iterItem;
+	for (iterItem = elements.begin(); iterItem != elements.end(); ++iterItem)
+	{
+		ShowItem(*iterItem);
+	}
+	return 0;
+}
+
+int GetItemValueByKey(std::map<int, std::vector<std::string> >& item, std::string itemKey, std::string& itemValue)
+{
+	std::map<int, std::vector<std::string> >::iterator iterData;
+	for (iterData = item.begin(); iterData != item.end(); ++iterData) {
+		std::vector<std::string>::iterator iterWord;
+		iterWord = iterData->second.begin();
+		if (*iterWord == itemKey) {
+			++iterWord;
+			itemValue = *iterWord ;
+		}
+	}
+	return 0;
+}
+
+int ParseElement(std::map<int, std::vector<std::string> >& data, std::vector<std::map<int, std::vector<std::string> > >& elements)
+{
+	std::string elementKeyStart = "[FORMAT]";
+	std::string elementKeyEnd = "[/FORMAT]";
+
+	std::map<int, std::vector<std::string> > item;
+	std::map<int, std::vector<std::string> >::iterator iterData;
+	for (iterData = data.begin(); iterData != data.end(); ++iterData)
+	{
+		std::vector<std::string>::iterator iterWord = iterData->second.begin();
+		if (*iterWord == elementKeyStart) {
+			item.clear();
+		} else if (*iterWord == elementKeyEnd) {
+			elements.push_back(item);
+		} else {
+			item.insert(*iterData);
+		}
+	}
+	return 0;
+}
+
+int GetVideoTimeLen(char *iniInfo, std::string videoFile)
+{
+	char opt = '=';
+	std::string duration;
+	std::map<int, std::vector<std::string> > data;
+	std::vector<std::map<int, std::vector<std::string> > > elements;
+
+	ParseFile(iniInfo, data, opt);
+	ParseElement(data, elements);
+	ShowElement(elements);
+	std::vector<std::map<int, std::vector<std::string> > >::iterator iterItem;
+
+	for (iterItem = elements.begin(); iterItem != elements.end(); ++iterItem)
+	{
+		std::map<int, std::vector<std::string> >::iterator iterLine = iterItem->begin();
+		std::vector<std::string>::iterator iterWord = iterLine->second.begin();
+		if (++iterWord != iterLine->second.end() && videoFile == *iterWord) {
+			GetItemValueByKey(*iterItem, videoFile, duration);
+			char* nptr = NULL;
+			return std::strtold(duration.c_str(), &nptr);
+		}
+	}
+	return 0;
+}
